f0e682ab546134aa160a26bc84d19017158444c5
faiqhaidar1 <faiqhaidar1@gmail.com>
2023-05-23 15:05:15 +0700

    database

--- semiclonshop.sql
+++ semiclonshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 22, 2023 at 05:02 AM
+-- Generation Time: May 23, 2023 at 10:04 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 7.4.33
 
@@ -42,7 +42,8 @@ INSERT INTO `category` (`id`, `slug`, `title`) VALUES
 (2, 'console', 'Console'),
 (3, 'games-console', 'Games Console'),
 (4, 'laptop', 'Laptop'),
-(5, 'mouse-gaming', 'mouse gaming');
+(8, 'komputer-rakitan', 'Komputer Rakitan'),
+(9, 'lain-lain', 'Lain Lain');
 
 -- --------------------------------------------------------
 
@@ -66,11 +67,11 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `id_category`, `slug`, `title`, `description`, `price`, `is_available`, `image`) VALUES
-(1, 1, 'new-iphone-10', 'new iphone 10', 'smartphone from Apple', 6000000, 1, 'smartphone.png'),
-(2, 4, 'new-asus-laptop', 'new asus laptop', 'laptop terbaru dari asus', 4000000, 1, 'laptop.png'),
-(3, 2, 'nintendo-switch-lite', 'Nintendo Switch Lite', 'new switch lite', 2900000, 1, 'nintendo-switch-lite-20230519101419.jpg'),
-(7, 2, 'nintendo-switch-oled', 'Nintendo Switch Oled', 'New Switch Oled', 4900000, 1, 'nintendo-switch-oled-20230519102443.jpg'),
-(8, 2, 'nintendo-switch-v2', 'Nintendo Switch V2', 'New Switch V2', 3900000, 1, 'nintendo-switch-v2-20230519102642.jpg');
+(12, 2, 'nintendo-switch-lite', 'Nintendo Switch Lite', 'Nintendo Switch Lite', 2900000, 1, 'nintendo-switch-lite-20230523130217.jpg'),
+(13, 2, 'nintendo-switch-v2', 'Nintendo Switch V2', 'Nintendo Switch V2', 3900000, 1, 'nintendo-switch-v2-20230523130233.jpg'),
+(15, 2, 'nintendo-switch-oled', 'Nintendo Switch Oled', 'Nintendo Switch Oled', 4900000, 1, 'nintendo-switch-oled-20230523133208.jpg'),
+(16, 4, 'asus-rog-laptop', 'Asus Rog Laptop', 'New Asus Rog Laptop', 8000000, 0, 'asus-rog-laptop-20230523133232.jpg'),
+(17, 2, 'playstation-4', 'Playstation 4', 'Playstation 4 Pro', 4000000, 1, 'playstation-4-20230523133613.jpg');
 
 -- --------------------------------------------------------
 
@@ -84,19 +85,20 @@ CREATE TABLE `user` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` enum('admin','member') NOT NULL,
-  `is_active` tinyint(1) NOT NULL DEFAULT 1
+  `is_active` tinyint(1) NOT NULL DEFAULT 1,
+  `image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `name`, `email`, `password`, `role`, `is_active`) VALUES
-(2, 'faiq', 'faiq@gmail.com', '$2y$10$zLaNU/2jjRG5fEd5TKwyoeqgObzRMSPJayevL5ngDbqG7jYUqrDCO', 'member', 1),
-(3, 'faiq', 'faiq1@gmail.com', '$2y$10$jPnc22WtvGSKS9R1PWIE8Oo8vzGWnZpoEaOBD0VwQ2JORLlphU7b.', 'member', 1),
-(4, 'faiq haidar', 'haidar@gmail.com', '$2y$10$6JrkDTcYXlnlGJAEVscqXOadLesjjDDgMru1yDMJzYBgQkyOR08Uu', 'member', 1),
-(5, 'Ali', 'ali@gmail.com', '$2y$10$09GTuLjHNM6VM0xYg4IB8O07Lfh.ocdK9VR7tyYPMcgoCaLz2tu.u', 'member', 1),
-(6, 'Ali', 'ali1@gmail.com', '$2y$10$S8ngrPxrDldDHUCz7IraeevdVSjzXwLcpz6q6RPxBMJGC08jESoRG', 'member', 1);
+INSERT INTO `user` (`id`, `name`, `email`, `password`, `role`, `is_active`, `image`) VALUES
+(2, 'faiq', 'faiq@gmail.com', '$2y$10$zLaNU/2jjRG5fEd5TKwyoeqgObzRMSPJayevL5ngDbqG7jYUqrDCO', 'member', 1, ''),
+(3, 'faiq', 'faiq1@gmail.com', '$2y$10$jPnc22WtvGSKS9R1PWIE8Oo8vzGWnZpoEaOBD0VwQ2JORLlphU7b.', 'member', 1, ''),
+(4, 'faiq haidar', 'haidar@gmail.com', '$2y$10$6JrkDTcYXlnlGJAEVscqXOadLesjjDDgMru1yDMJzYBgQkyOR08Uu', 'member', 1, ''),
+(5, 'Ali', 'ali@gmail.com', '$2y$10$09GTuLjHNM6VM0xYg4IB8O07Lfh.ocdK9VR7tyYPMcgoCaLz2tu.u', 'member', 1, ''),
+(6, 'Ali', 'ali1@gmail.com', '$2y$10$S8ngrPxrDldDHUCz7IraeevdVSjzXwLcpz6q6RPxBMJGC08jESoRG', 'member', 1, '');
 
 --
 -- Indexes for dumped tables
@@ -128,13 +130,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `user`
